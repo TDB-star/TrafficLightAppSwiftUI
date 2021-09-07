fcd0fcd8eac47712aef0db642ce83db47e7cac86
TDB-star <tatiana.blache@gmail.com>
2021-09-07 11:05:40 +0200

    Initial Commit

---
+++ TrafficLightAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightAppSwiftUI
+//
+//  Created by Tatiana Dmitrieva on 07/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightAppSwiftUI/TrafficLightAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppSwiftUIApp.swift
+//  TrafficLightAppSwiftUI
+//
+//  Created by Tatiana Dmitrieva on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
